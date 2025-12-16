0820df2b340b29cd80f2df35e39361c438096905
Qi-Month <dy757410@163.com>
2025-12-16 19:01:08 +0800

    配置文件更新, 支持fluidTags

--- src/main/java/top/nebula/utils/mixin/FluidTypeMixin.java
+++ src/main/java/top/nebula/utils/mixin/FluidTypeMixin.java
@@ -1,12 +1,16 @@
 package top.nebula.utils.mixin;
 
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.fluids.FluidType;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,23 +19,56 @@ import top.nebula.utils.config.CommonConfig;
 
 @Mixin(value = FluidType.class, remap = false)
 public abstract class FluidTypeMixin {
-	@Shadow(remap = false)
-	public abstract String getDescriptionId();
+	/**
+	 * 判断当前 FluidState 是否在 burningFluids 配置中
+	 * 支持：
+	 * - "modid:fluid"
+	 * - "#namespace:tag"
+	 */
+	@Unique
+	private static boolean nebula$isBurningFluid(FluidState state) {
+		Fluid fluid = state.getType();
+		ResourceLocation id = BuiltInRegistries.FLUID.getKey(fluid);
+
+		for (String entry : CommonConfig.BURNING_FLUIDS.get()) {
+			if (entry.startsWith("#")) {
+				ResourceLocation tagId = ResourceLocation.tryParse(entry.substring(1));
+				if (tagId != null) {
+					TagKey<Fluid> tag = TagKey.create(
+							BuiltInRegistries.FLUID.key(),
+							tagId
+					);
+					if (fluid.is(tag)) {
+						return true;
+					}
+				}
+			} else {
+				if (id.toString().equals(entry)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
 	@Inject(method = "move", at = @At("HEAD"), remap = false)
-	public void move(FluidState state, LivingEntity entity, Vec3 movementVector, double gravity, CallbackInfoReturnable<Boolean> cir) {
-		String[] idComponents = this.getDescriptionId().split("\\.");
-		String fluidID = idComponents[1] + ":" + idComponents[2];
-		if (CommonConfig.BURNING_FLUIDS.get().contains(fluidID)) {
+	private void nebula$move(
+			FluidState state,
+			LivingEntity entity,
+			Vec3 movementVector,
+			double gravity,
+			CallbackInfoReturnable<Boolean> cir
+	) {
+		if (nebula$isBurningFluid(state)) {
 			entity.lavaHurt();
 		}
 	}
 
 	@Inject(method = "setItemMovement", at = @At("HEAD"), remap = false)
-	public void setItemMovement(ItemEntity entity, CallbackInfo ci) {
-		String[] idComponents = this.getDescriptionId().split("\\.");
-		String fluidID = idComponents[1] + ":" + idComponents[2];
-		if (CommonConfig.BURNING_FLUIDS.get().contains(fluidID)) {
+	private void nebula$setItemMovement(ItemEntity entity, CallbackInfo ci) {
+		FluidState state = entity.level().getFluidState(entity.blockPosition());
+
+		if (nebula$isBurningFluid(state)) {
 			entity.lavaHurt();
 		}
 	}
