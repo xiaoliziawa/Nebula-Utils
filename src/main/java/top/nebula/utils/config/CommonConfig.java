9e50133c3d860c516b4c925db84dd4a6b9dc6760
Qi-Month <dy757410@163.com>
2026-01-04 10:52:43 +0800

    BUILDER => builder

--- src/main/java/top/nebula/utils/config/CommonConfig.java
+++ src/main/java/top/nebula/utils/config/CommonConfig.java
@@ -5,27 +5,27 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import java.util.List;
 
 public class CommonConfig {
-	private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+	private static final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 
 	public static final ForgeConfigSpec.ConfigValue<List<? extends String>> BURNING_FLUIDS;
 	public static final ForgeConfigSpec.BooleanValue ENABLE_LARGE_SPRUCE_PODZOL_CONVERSION;
 
 	static {
-		BUILDER.comment("All settings below will only take effect after restarting the server or client.")
+		builder.comment("All settings below will only take effect after restarting the server or client.")
 				.push("general");
 
-		BURNING_FLUIDS = BUILDER
+		BURNING_FLUIDS = builder
 				.comment("Fluids in this list can burn entities like lava")
 				.comment("supports fluid tags, declared with \"#\"")
 				.defineListAllowEmpty("burningFluids", List.of("#forge:molten_materials"), CommonConfig::validateString);
 
-		ENABLE_LARGE_SPRUCE_PODZOL_CONVERSION = BUILDER
+		ENABLE_LARGE_SPRUCE_PODZOL_CONVERSION = builder
 				.comment("Whether to enable the feature that converts surrounding dirt to podzol when large spruce trees grow")
 				.comment("Default value: false")
 				.define("enableLargeSprucePodzolConversion", false);
 	}
 
-	public static final ForgeConfigSpec SPEC = BUILDER.build();
+	public static final ForgeConfigSpec SPEC = builder.build();
 
 	/**
 	 * 云杉木控制
