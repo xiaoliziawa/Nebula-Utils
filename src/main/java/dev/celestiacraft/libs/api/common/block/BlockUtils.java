21b69bf385dd2fabd7d6067e3194868a1495c2bb
Qi-Month <dy757410@163.com>
2026-05-20 23:06:43 +0800

    isInStructure

--- src/main/java/dev/celestiacraft/libs/api/common/block/BlockUtils.java
+++ src/main/java/dev/celestiacraft/libs/api/common/block/BlockUtils.java
@@ -7,7 +7,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.levelgen.structure.Structure;
 
 public class BlockUtils {
-	public static boolean isInFortress(Level level, BlockPos pos, ResourceKey<Structure> structure) {
+	public static boolean isInStructure(Level level, BlockPos pos, ResourceKey<Structure> structure) {
 		if (!(level instanceof ServerLevel serverLevel)) {
 			return false;
 		}
